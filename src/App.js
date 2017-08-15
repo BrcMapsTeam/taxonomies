b66e08901f954fd1a800808a3e9bb26d2d6fadf5
Zibethin <nguyenththuong@gmail.com>
2017-08-15 16:50:42 +0100

    Added basic UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+let content = ["Please enter the link to your data:", 
+    "Please select the #crisis column you wish to map:",
+    "Which Taxonomy are you mapping FROM?",
+    "Which Taxonomy are you mapping TO?",
+    "The following term was not mappable.",
+    "We have mapped the following terms, here is the HXL replacement map for it:",
+    "Success your dataset has been converted, select the format you wish to download it in:"
+]
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Taxonomy React template!
-        </p>
-      </div>
-    );
-  }
+    renderPage(i){
+        return (
+            <Page value={i} />
+        );
+            }
+
+    render() {
+            return (
+              <div className="App">
+                {this.renderPage(0)}
+                {this.renderPage(1)}
+                {this.renderPage(2)}
+                {this.renderPage(3)}
+                {this.renderPage(4)}
+                {this.renderPage(5)}
+                {this.renderPage(6)}
+              </div>
+            );
+    }
 }
+
+class Page extends Component {
+    render() {
+        const previousPage = this.props.value-1;
+        const nextPage = this.props.value + 1;
+
+        return(
+            <div id={"Page"+this.props.value} className = "Page">
+                <div className="PageContent">
+                {content[this.props.value]}
+                </div>
+                <NavButton value="Back" url={'#Page'+ previousPage}/>
+                <NavButton value="Next" url={'#Page'+ nextPage}/>
+            </div>
+            );
+            }
+}
+
+class NavButton extends Component {
+    render() {
+        return(
+            <a className = "NavButton" href={this.props.url}>
+            {this.props.value}
+            </a>
+            );
+    }
+}
+
+//class entryBox extends Component {
+//    render() {
+//        return(
+//            <>
+//            );
+//    }
+//}
 
 export default App;
