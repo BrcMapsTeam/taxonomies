b2b7de1420fe5ca7c1f1035160bf90e14dbb1513
Zibethin <nguyenththuong@gmail.com>
2017-08-22 13:42:05 +0100

    changed container to flex-row

--- src/1_upload_page/UploadPage.js
+++ src/1_upload_page/UploadPage.js
@@ -5,10 +5,15 @@ import '../App.css';
 class UploadPage extends Component {
 
     render(){
-        return (<div>
-                    <h1>Upload Page</h1>
-                    <FormInput />
-                    <div className="container">
+        return (<div className="flex-page">
+                    <h1 className="flex-row">Upload Page</h1>
+                    <div className="flex-row">
+                        <div>Link to dataset you wish to map:</div>
+                    </div> 
+                    <div className="flex-row">
+                        <FormInput />
+                    </div>
+                    <div className="flex-row">
                         <div className="NavButtonGreyed">Back</div>
                         <div className="NavButton" onClick={ this.props.nextStep() }>Next</div>
                     </div>
@@ -20,10 +25,9 @@ class FormInput extends Component {
 
     render(){
         return (<div>
-                    <input type="text" name="link" /><br /> 
+                    <input type="text" name="link" id="data-link" /><br /> 
                     <div className="FormLine"></div>
                 </div>);
-
     }
 }
 
