57bf4c74c3fe17a16dc0cdd61405985fa2b3e137
Zibethin <nguyenththuong@gmail.com>
2017-09-06 15:16:20 +0100

    added table with clickable tag

--- src/2_select_column_page/SelectColumnPage.js
+++ src/2_select_column_page/SelectColumnPage.js
@@ -2,21 +2,137 @@ import React, { Component } from 'react';
 //import ReactDOM from 'react-dom';
 import '../App.css';
 
+
+let loadingMessage = <tr><td>Loading...</td></tr>;
+
 class SelectColumnPage extends Component {
-    goBack(e) {
-        e.preventDefault();
-        this.props.previousStep();
+    constructor(props){
+        super(props);
+        this.state =  {data: loadingMessage};
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({data: nextProps.data});
     }
 
     render(){
-        return <div><h1>Column</h1>
-            <div className="container">
-              <div className="NavButton" onClick={this.props.previousStep()}>Back</div>
-              <div className="NavButtonGreyed">Next</div>
-            </div>
-            </div>;
+        return (<div className="flex-page">
+                    <h2 className="flex-row">Disaster Taxonomies</h2>
+                    <div className="flex-row">
+                        <div>Please select the <b>#crisis</b> column you wish to map:</div>
+                    </div> 
+                    <div className="flex-row">
+                        <DataTable data={this.props.data} handleTagChange={this.props.handleTagChange}/>
+                    </div>
+                    <div className="flex-row">
+                         <div className="NavButton" onClick={this.props.previousStep()}>Back</div>
+                         <div className="NavButtonGreyed">Next</div>
+                    </div>
+                </div>);
     }
 }
 
+
+class DataTable extends Component {
+    constructor(props){
+        super(props);
+        this.state =  {data: loadingMessage};
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({data:nextProps.data});
+    }
+
+    // Object should contain: data, start and end of rows to be processed, th/td/other tags
+    // e.g.: parseData({data:data, rowStart:0, rowEnd:10})
+
+    parseData(obj){
+        obj.data = obj.data.slice(obj.rowStart, obj.rowEnd); //Taking top 7 lines
+
+        let finalTable =
+        obj.data.map( function(row, i){
+            const temp = row.map(function(item, i){ 
+                return <td key={i}>{item}</td>
+            });
+            return <tr key={i}>{temp}</tr>;
+        }
+        );
+        return finalTable;
+    }
+
+    parseTag(obj){
+
+        obj.data = obj.data.slice(obj.rowStart, obj.rowEnd); //Taking top 7 lines
+        let finalTable =
+        obj.data.map( function(row, i){
+            console.log("fahfkhaf=", this);
+            const temp = row.map(function(item, i){ 
+                return <td key={i} id={item} className="clickable-cell" onClick={this.props.handleTagChange} >{item}</td>
+            }, this //binds this inside of anonymous function
+            );
+            return <tr key={i}>{temp}</tr>;
+        }, this //binds this inside of anonymous function
+        );
+        return finalTable;
+    }
+    // onClick={this.props.handleTagChange}
+
+    render(){
+
+        let userData = this.state.data;
+        let userData2 = "";
+        let userData3 = "";
+
+        if (userData !== loadingMessage && Object.keys(userData).length !== 0) {
+            console.log(Object.keys(userData).length !== 0);
+            const headerParameters = {
+                'data': userData,
+                'rowStart': 0,
+                'rowEnd': 1
+            };
+            const tagParameters = {
+                'data': userData,
+                'rowStart': 1,
+                'rowEnd': 2
+            };
+            const dataParameters = {
+                'data': userData,
+                'rowStart': 2,
+                'rowEnd': 6
+            };
+
+            userData = this.parseData(headerParameters);
+            userData2 = this.parseTag(tagParameters);
+            userData3 = this.parseData(dataParameters);
+        } //end if
+
+        return(
+            <table className="scrollable-table">
+                <tbody>
+                    {userData}
+                    {userData2}
+                    {userData3}
+                </tbody>
+            </table>
+            );
+    }
+}
+
+
+
+
+
+//class tableCell extends Component {
+
+//    render(){
+//        return(
+//            <>
+//        );
+//    }
+//}
+
+//dataForTable.map(row =>
+//    <tr>{row}</tr>
+//});
 
 export default SelectColumnPage;
--- src/App.js
+++ src/App.js
@@ -8,23 +8,38 @@ import SelectColumnPage from './2_select_column_page/SelectColumnPage';
 
 class App extends Component {
 
-    // Sets default page state as 1
+    // ------------------ CONSTRUCTOR ---------------------------
+
     constructor(props){
         super(props);
         this.state = {
             step: 1,
-            value: "",
-            data: ""
+            value: "https://proxy.hxlstandard.org/data.json?url=https%3A//docs.google.com/spreadsheets/d/1x30JatnFHEqEQx2Nm9NQhlHlYL2Kml-84v9plgO1RcY/edit%23gid%3D1339100966",
+            data: "Loading...",
+            crisisTag: "nothing",
+            mapFrom: "IFRC",
+            mapTo: "GLIDE"
         };
 
+        //List of all functions in the App Class using "this"
         this.handleLinkChange = this.handleLinkChange.bind(this);
+        this.handleTagChange = this.handleTagChange.bind(this);
         this.getUserLink = this.getUserLink.bind(this);
+        this.nextAndSave = this.nextAndSave.bind(this);
+        this.previousStep = this.previousStep.bind(this);
+        this.nextStep = this.nextStep.bind(this);
     }
+
+
+    // ------------------ FUNCTIONS ---------------------------
 
     handleLinkChange(event) {
         this.setState({value: event.target.value});
     }
 
+    handleTagChange(event) {
+        this.setState({crisisTag: event.target.id});
+    }
 
     //ajax request to get json data
     getUserLink(event) {
@@ -48,9 +63,9 @@ class App extends Component {
             });
         };
 
-        getJSON(userLink).then(function(data) {
-            this.setState({data:data});
-            console.log(this);
+        getJSON(userLink).then(function(userData) {
+            console.log("e=",this);
+            this.setState({data:userData});
         }.bind(this), function(status) { //error detection....
             alert('Something went wrong.');
         });
@@ -71,40 +86,58 @@ class App extends Component {
         })
     }
 
+    // Same as nextStep but also saves the user data as state
     nextAndSave(){
         this.nextStep();
         this.getUserLink();
     }
-    // function to show content of page
+
+    getCrisisTag(){
+
+    }
+
+
+    //----------------- function to show content of page -----------------
 
     showStep() {
         switch (this.state.step){
-            case 1: 
-                return (<UploadPage nextAndSave={this.nextAndSave.bind(this)} 
-                                    handleLinkChange={this.handleLinkChange.bind(this)} 
-                                    link={this.state.value} 
-                                    getUserLink = {this.getUserLink.bind(this)} />);
 
+            /*---------------------- PAGE 1 --------------------------*/
+            case 1: 
+                return (
+                    <UploadPage 
+                        nextAndSave={this.nextAndSave} 
+        handleLinkChange={this.handleLinkChange} 
+        link={this.state.value} 
+        getUserLink = {this.getUserLink} 
+     />);
+
+        /*---------------------- PAGE 2 ---------------------------*/
             case 2:
                 return (<SelectColumnPage 
-                            previousStep={() => this.previousStep.bind(this)} 
-                            nextStep
+                            previousStep={() => this.previousStep}
+                            handleTagChange = {this.handleTagChange}
+                            data = {this.state.data}
+                            crisisTag = {this.state.crisisTag}
                         />);
+
+    /*---------------------- ERROR PAGE -----------------------*/
             default: 
                 return (<h1>Error, please check your entries are correct.</h1>);
 }
 }
 
-    // Rendering "App"
+    // ------------------ Rendering "App" ------------------------------
+
     render() {
         return(
         <div className="App">
+{this.showStep()}
             <div className="progress-step">Step {this.state.step}</div>
-            {this.showStep()}
         </div>
         );
 }
-}
+} //END CLASS APP
 
 
 
