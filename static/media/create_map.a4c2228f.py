c4e14ca20a5a5c3d71f2b1ed660adc2d13edaa52
Zibethin <nguyenththuong@gmail.com>
2018-01-04 15:59:28 +0000

    Updates

---
+++ static/media/create_map.a4c2228f.py
@@ -0,0 +1,70 @@
+# Note: Script will replace existing taxonomy maps
+
+import urllib, json
+
+listOfTaxonomies = "https://docs.google.com/spreadsheets/d/1_EKu8sGBryuZY9smHMemrRuZ6EvvqoTxsW_tbkS9f74/edit#gid=0";
+
+def createHXLLink(url):
+    return "https://proxy.hxlstandard.org/data.json?strip-headers=on&url=" + urllib.quote(url)
+
+
+response = urllib.urlopen(createHXLLink(listOfTaxonomies))
+data = json.loads(response.read())
+taxonomies = []
+index = 0
+                          
+for row in data:
+    
+    # Removes scenario when there is no URL provided
+    if index > 0 and len(row[5])>2:
+        name = row[3]+"_"+row[2]+row[1]
+        urlOnline = row[5]
+        taxonomies.append({"name": name, "urlOnline": urlOnline})
+    index += 1
+                          
+
+for taxonomy in taxonomies:
+    
+    print "Processing taxonomy: ", taxonomy['name']
+
+    taxonomy['url'] = 'taxonomy_maps/'+taxonomy['name']+'_base.json'
+    hxlProxyURL = createHXLLink(taxonomy['urlOnline'])
+
+    try:
+        response = urllib.urlopen(hxlProxyURL)
+        data = json.loads(response.read())
+        i = 0
+        output = {}
+
+        for row in data:
+            subTerm = row[1]
+            baseTerm = row[2]
+
+            if i>0:
+                if subTerm not in output:
+                    output[subTerm] = [baseTerm]
+                else:
+                    output[subTerm].append(baseTerm)
+            i=i+1
+
+        with open('../'+taxonomy['url'], 'w') as outfile:
+            json.dump(output, outfile)
+    except Exception as error:
+        print "Error processing the taxonomy: ", taxonomy['name']
+        print error
+            
+            
+# Creating/Updating the config.js file
+
+
+with open('../taxonomy_maps/config.js', 'w') as file:
+    file.write("let config_json = \n")
+    
+with open('../taxonomy_maps/config.js', 'a') as file:
+    json.dump(taxonomies, file)
+    file.write(";\nexport default config_json;")
+
+
+    
+    
+    
