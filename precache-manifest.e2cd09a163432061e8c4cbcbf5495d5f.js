f77f52107e3fd0034de94c2f80705753aa1b381e
Zibethin <nguyenththuong@gmail.com>
2020-04-06 14:19:17 +0100

    Updates

---
+++ precache-manifest.e2cd09a163432061e8c4cbcbf5495d5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84a641c246c5ed1a35751a8e5661390",
+    "url": "/taxonomies/index.html"
+  },
+  {
+    "revision": "44c487fa59f4af44331a",
+    "url": "/taxonomies/static/css/main.234d3f99.chunk.css"
+  },
+  {
+    "revision": "bce8015d58bbe5cedba2",
+    "url": "/taxonomies/static/js/2.d7abdf29.chunk.js"
+  },
+  {
+    "revision": "a3ef6a11e11ca6a16b435c3d2be41ee9",
+    "url": "/taxonomies/static/js/2.d7abdf29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c487fa59f4af44331a",
+    "url": "/taxonomies/static/js/main.b7332024.chunk.js"
+  },
+  {
+    "revision": "25c567d21d69a4e62484",
+    "url": "/taxonomies/static/js/runtime-main.830d2582.js"
+  },
+  {
+    "revision": "dafa1afe153d155032a0ad1159db9c5e",
+    "url": "/taxonomies/static/media/App.dafa1afe.less"
+  },
+  {
+    "revision": "76562cd4442137091fdb2862029414ab",
+    "url": "/taxonomies/static/media/create_map.76562cd4.py"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/taxonomies/static/media/logo.ee7cd8ed.svg"
+  }
+]);
